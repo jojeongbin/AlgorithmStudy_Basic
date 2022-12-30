8ca452bceff0853b2fb8cf3a513e143505c271a0
jojeongbin <cjb1009@naver.com>
2022-12-31 02:15:18 +0900

    나이 출력

---
+++ programmers/src/java/age.java
@@ -0,0 +1,12 @@
+package java;
+
+public class age {
+        public int solution(int age) {
+            int answer = 0;
+            int birth=0;
+            birth=2022-(age-1);
+            answer=birth;
+
+            return answer;
+        }
+}
