4acada545ec05d77eb4909b69620ca16164bbf65
jojeongbin <cjb1009@naver.com>
2023-01-01 19:16:04 +0900

    피자 나눠 먹기(1)

---
+++ programmers/src/java/pizza.java
@@ -0,0 +1,14 @@
+package java;
+
+public class pizza {
+    public int solution(int n) {
+        int answer = 0;
+        if(n%7==0){
+            answer=n/7;
+        }
+        else{
+            answer=(n/7)+1;
+        }
+        return answer;
+    }
+}
