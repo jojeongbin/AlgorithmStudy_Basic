d547f9dd1f2a3014024f459f3ad048b249a470b6
jojeongbin <cjb1009@naver.com>
2023-01-02 17:52:19 +0900

    배열 두 배 만들기

--- programmers/src/java/doubleArray.java
+++ programmers/src/java/doubleArray.java
@@ -1,2 +1,11 @@
-package java;public class doubleArray {
+package java;
+
+public class doubleArray {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        for(int i=0; i<numbers.length;i++){
+            answer[i]=numbers[i]*2;
+        }
+        return answer;
+    }
 }
