caff14acac47e6c4d844db7158ca92f7285f958f
jojeongbin <cjb1009@naver.com>
2023-01-02 17:51:54 +0900

    편지(편지 길이 계산)

--- programmers/src/java/message.java
+++ programmers/src/java/message.java
@@ -1,2 +1,9 @@
-package java;public class message {
+package java;
+
+public class message {
+    public int solution(String message) {
+        int answer = 0;
+        answer=2*message.length();
+        return answer;
+    }
 }
