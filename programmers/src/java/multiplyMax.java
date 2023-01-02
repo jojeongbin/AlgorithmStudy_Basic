813858c4b9098b6599b4c1666959f9da38800aff
jojeongbin <cjb1009@naver.com>
2023-01-02 17:51:24 +0900

    최대값 만들기(1)

--- programmers/src/java/multiplyMax.java
+++ programmers/src/java/multiplyMax.java
@@ -1,2 +1,13 @@
-package java;public class multiplyMax {
+package java;
+
+import java.util.Arrays;
+
+public class multiplyMax {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+
+        answer=numbers[numbers.length-1]*numbers[numbers.length-2];
+        return answer;
+    }
 }
