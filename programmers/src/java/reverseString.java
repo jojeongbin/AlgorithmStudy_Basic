425525298397e95b6657a22fd75b854c30a8dc8e
jojeongbin <cjb1009@naver.com>
2023-01-02 17:51:04 +0900

    문자열 뒤집기

--- programmers/src/java/reverseString.java
+++ programmers/src/java/reverseString.java
@@ -1,2 +1,14 @@
-package java;public class reverseString {
+package java;
+
+public class reverseString {
+    public String solution(String my_string) {
+    String answer="";
+        for(int i=my_string.length()-1 ; i>=0;i--)
+    {
+        answer+=my_string.substring(i,i+1);
+    }
+
+
+        return answer;
+}
 }
