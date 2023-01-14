743bb58a5077958279888dc72f7d2a6ece1a66da
jojeongbin <cjb1009@naver.com>
2023-01-14 17:00:47 +0900

    자릿수 더하기 : Integer.parseInt/substring/Integer.toString

---
+++ programmers/src/java/AddDigits.java
@@ -0,0 +1,13 @@
+package java;
+
+public class AddDigits {
+    public int solution(int n) {
+        int answer = 0;
+        String number=Integer.toString(n);
+
+        for(int i=0;i<number.length();i++){
+            answer+=Integer.parseInt(number.substring(i,i+1));
+        }
+        return answer;
+    }
+}
