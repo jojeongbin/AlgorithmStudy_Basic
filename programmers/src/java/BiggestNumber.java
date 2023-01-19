3674098687e4ce1b99e6466d2909f467af1c78aa
jojeongbin <cjb1009@naver.com>
2023-01-19 23:07:04 +0900

    가장 큰 수 찾기

---
+++ programmers/src/java/BiggestNumber.java
@@ -0,0 +1,20 @@
+package java;
+
+public class BiggestNumber {
+    class Solution {
+        public int[] solution(int[] array) {
+            int[] answer = new int[2];
+            int max=array[0];
+            for(int i=0;i<array.length;i++){
+                if(array[i]>max){
+                    max=array[i];
+                    answer[0]=array[i];
+                    answer[1]=i;
+                }
+            }
+
+
+            return answer;
+        }
+    }
+}
