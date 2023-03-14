8054a6867242b650c90d7670aec66d7dc5518c3d
jojeongbin <cjb1009@naver.com>
2023-03-14 18:11:18 +0900

    피자나눠먹기(2) :최소공배수찾기

---
+++ programmers/src/java/Pizza2.java
@@ -0,0 +1,24 @@
+package java;
+
+public class Pizza2 {
+    public int solution(int n) {
+        int slice = 6;
+        int lcm = (n * slice) / gcd(n, 6);
+        return lcm / slice;
+    }
+
+    public static int gcd(int a, int b) {
+        if (a <= b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (b == 0) {
+            return a;
+        }
+
+        return gcd(b, (a % b));
+    }
+}
+
