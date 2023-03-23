cad35d53c42331417617334c6eb324a4b137022b
jojeongbin <cjb1009@naver.com>
2023-03-23 17:43:28 +0900

    약수 구하기

--- programmers/src/java/Measure.java
+++ programmers/src/java/Measure.java
@@ -1,2 +1,19 @@
-package java;public class Measure {
+package java;
+import java.util.*;
+
+class Measure {
+    public int[] solution(int n) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer.add(i);
+            }
+        }
+        int[] result = new int[answer.size()];
+        for (int i = 0; i < answer.size(); i++) {
+            result[i] = answer.get(i);
+        }
+        Arrays.sort(result);
+        return result;
+    }
 }
